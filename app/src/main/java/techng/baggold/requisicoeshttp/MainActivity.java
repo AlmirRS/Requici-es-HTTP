9d25378c12a7bb9a34029799e84b5d6c15a77087
Almir Ramos <93881716+AlmirRS@users.noreply.github.com>
2022-11-17 16:02:03 -0300

    commit number two

--- app/src/main/java/techng/baggold/requisicoeshttp/MainActivity.java
+++ app/src/main/java/techng/baggold/requisicoeshttp/MainActivity.java
@@ -2,10 +2,19 @@ package techng.baggold.requisicoeshttp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +34,62 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                MyTask task = new MyTask();
+                String urlApi = "https://blockchain.info/ticker";
+                task.execute(urlApi);
+
             }
         });
+    }
+
+    class MyTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String stringUrl = strings[0];
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader =  null;
+            StringBuffer buffer = null;
+
+            try {
+                URL url = new URL( stringUrl );
+                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+
+                // Recuper os dados em Bytes
+                inputStream = conexao.getInputStream();
+
+                //inputStreamReader le os dados em Bytes e decodifica para caracteres
+                inputStreamReader = new InputStreamReader(inputStream);
+
+                //Objeto utilizado para a leitura dos caracteres do inputStreamReader
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String linha = "";
+
+                while ((linha = reader.readLine()) != null) {
+
+                }
+
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return inputStreamReader.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+            super.onPostExecute(resultado);
+            textResultado.setText(resultado);
+        }
     }
 }
